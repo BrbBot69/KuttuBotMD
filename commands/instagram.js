90009e5f786cbf63d0eb3c4fcbeec7feeb43ee8f
Gᴏᴜᴛʜᴀᴍ Jᴏsʜ 么 <97418751+GouthamSER@users.noreply.github.com>
2025-09-05 14:12:23 +0530

    Update instagram.js

--- commands/instagram.js
+++ commands/instagram.js
@@ -113,12 +113,12 @@ async function instagramCommand(sock, chatId, message) {
                     await sock.sendMessage(chatId, {
                         video: { url: mediaUrl },
                         mimetype: "video/mp4",
-                        caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝗞𝗡𝗜𝗚𝗛𝗧-𝗕𝗢𝗧"
+                        caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 Gᴏᴜᴛʜᴀᴍ Jᴏsʜ 么"
                     }, { quoted: message });
                 } else {
                     await sock.sendMessage(chatId, {
                         image: { url: mediaUrl },
-                        caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝗞𝗡𝗜𝗚𝗛𝗧-𝗕𝗢𝗧"
+                        caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 Gᴏᴜᴛʜᴀᴍ Jᴏsʜ 么"
                     }, { quoted: message });
                 }
                 
