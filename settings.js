76524164161f7b2925d1e1a02c5750b0ce76d39a
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-10-01 07:17:13 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'KUTTU_MD',
-  author: '‎Gᴏᴜᴛʜᴀᴍ Jᴏsʜ 么',
-  botName: "Kuttu Bot",
-  botOwner: 'Gᴏᴜᴛʜᴀᴍ Jᴏsʜ 么', // Your name
-  ownerNumber: '917034898741', //Set your number here without + symbol, just add country code & number without any space
+  packname: '★★★★♤',
+  author: '‎Gᴏᴜᴛʜᴀᴍ 么',
+  botName: "☆☆☆☆♠︎",
+  botOwner: 'Gᴏᴜᴛʜᴀᴍ 么', // Your name
+  ownerNumber: '27767494368', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
